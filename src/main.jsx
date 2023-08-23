f055962a60745f416fccbf33e8570ed436a68f3e
rohan future devoloper <xrohaislam373@gmail.com>
2023-08-24 04:04:49 +0600

    tanstack Query

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,23 @@ import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Provides/AuthProvider';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <AuthProvider>
    <HelmetProvider>
+   <QueryClientProvider client={queryClient}>
    <div className='max-w-7xl mx-auto'>
      <RouterProvider router={router} />
      </div>
+    </QueryClientProvider>
+   
    </HelmetProvider>
    </AuthProvider>
   </React.StrictMode>,
