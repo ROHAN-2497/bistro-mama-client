c5aebf081f334ad3f7e4d4d705883e8d7ed033e1
rohan future devoloper <xrohaislam373@gmail.com>
2023-08-31 03:30:18 +0600

    image url

--- src/Pages/DashBoard/AddItem/AddItem.jsx
+++ src/Pages/DashBoard/AddItem/AddItem.jsx
@@ -1,10 +1,29 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
 
+const img_hosting_token = import.meta.env.VITE_Image_Upload_Token;
 const AddItem = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
+  const onSubmit = data =>{
+  const formData = new FormData();
+  formData.append('image', data.image[0])
+  fetch(img_hosting_url, {
+    method: 'post',
+    body: formData
+  })
+  .then(res => res.json())
+  .then(imageResponse => {
+   if(imageResponse){
+    const imgURL = imageResponse.data.display_url;
+  const {name, price, recipe, category}  = data;
+  const newItem = {name, price: parseFloat(price), recipe, category, image:imgURL}
+  console.log(newItem);
+   }
+  })
+  console.log(data);
+  console.log(img_hosting_token)
+  console.log(errors);}
   return (
     <div className="w-full px-10">
       <SectionTitle subTitle="add an item" heading="whats news"></SectionTitle>
@@ -25,14 +44,15 @@ const AddItem = () => {
           <label className="label">
             <span className="label-text">Category</span>
           </label>
-          <select {...register("category", { required: true })} className="select select-primary w-full ">
-            <option disabled selected>
-              Category
+          <select defaultValue="Pick One" {...register("category", { required: true })} className="select select-primary w-full ">
+            <option disabled >
+              Pick One
             </option>
             <option>Pizza</option>
             <option>Salad</option>
             <option>Dessert</option>
             <option>Drinks</option>
+            <option>deshi</option>
           </select>
         </div>
         <div className="form-control w-full ">
